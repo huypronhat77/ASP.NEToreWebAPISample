82f44a6f4d1b65ac4b7a011bca3ea5df7a51b870
Huy Tran <huypronhat77@gmail.com>
2023-06-08 20:00:22 +0700

    Day 1 || Create basic API with methods GET POST PUT and DELETE

---
+++ WebAPISampleApp/WebAPISampleApp/Controllers/ProductController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPISampleApp.Model;
+
+namespace WebAPISampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Products);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var seletedProdut = Products.SingleOrDefault(p => p.Id.Equals(Guid.Parse(id)));
+
+                if (seletedProdut is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(seletedProdut);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            Product product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = productVM.Name,
+                Price = productVM.Price
+            };
+            Products.Add(product);
+
+            return Ok( new { Success= true, Data = product });
+        }
+
+        [HttpPut("id")]
+        public IActionResult Edit(string id, ProductVM editProduct)
+        {
+            try
+            {
+                var seletedProdut = Products.SingleOrDefault(p => p.Id.Equals(Guid.Parse(id)));
+
+                if (seletedProdut is null)
+                {
+                    return NotFound();
+                }
+
+
+                seletedProdut.Name = editProduct.Name;
+                seletedProdut.Price = editProduct.Price;
+
+                return Ok(new { Success = true, Data = seletedProdut});
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                var seletedProdut = Products.SingleOrDefault(p => p.Id.Equals(Guid.Parse(id)));
+
+                if (seletedProdut is null)
+                {
+                    return NotFound();
+                }
+
+                Products.Remove(seletedProdut);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ WebAPISampleApp/WebAPISampleApp/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPISampleApp.Model
+{
+    public class ProductVM
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Product : ProductVM
+    {
+        public Guid Id { get; set; }
+    }
+}
